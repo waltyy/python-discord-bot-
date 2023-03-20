142549783b442ca76bcd75182223efcc8247a7f9
Ashane De <100096985+waltyy@users.noreply.github.com>
2023-03-20 20:18:25 +0000

    Finished Joke function and added comments - Added comment about a possible Rock Paper Scissors function.

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
-import discord, random, os
+import discord, random, os, logging
+from jokeapi import Jokes
 from discord.ext import commands
+import utils
 
 TOKEN = os.environ['MY_TOKEN']
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix = "!", intents = intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 
-@bot.command(name = "roll")
-async def roll(ctx,num):
+@bot.command(name="roll")
+async def roll(ctx, num):
   for i in range(int(num)):
-    d = random.randint(1,6)
+    d = random.randint(1, 6)
     await ctx.channel.send(f"Rolling a D6 : {d}")
 
-@bot.command(name = "ask")
+
+@bot.command(name="ask")
 async def ask(ctx, *words):
   st = ""
   for word in words:
-    st+= word
+    st += word
   if st.endswith("?"):
     s = "cool question nerd"
     await ctx.channel.send(s)
@@ -27,9 +30,30 @@ async def ask(ctx, *words):
     await ctx.channel.send("That's not a question")
 
 
+@bot.command(
+  name="joke", aliases=["jokes", "j", "J"]
+)  #aliases means it run the funtion if the user spells joke with an s or if the user simply puts /j
+async def joke(ctx):
+  j = await Jokes()
+  '''
+  blacklist = ["racist"]
+  if not ctx.message.channel.is_nsfw():
+    blacklist.append("nsfw")
+    '''
+  #the blacklist code is from the jokeapi package and it makes sure that the "racist" jokes dont come up when it is in the blacklist list. The "blacklist.append" adds the "nsfw" to the blacklist list IF the channel is not nsfw which works with line 38. To make sure the blacklist code works i have to add (blacklist=blacklist) to line 42.
+  joke = await j.get_joke()
+  msg = ""
+  if joke["type"] == "single":
+    msg = joke["joke"]
+    #this code makes the joke in one line.
+  else:
+    msg = joke["setup"]
+    msg += f"||{joke['delivery']}||"
+    #this code makes the joke into two parts. the 2nd part will be blacked out and will need the user to click it to reveil the joke.
+  await ctx.send(msg)
+  #the jokes come from r/jokes on reddit.
 
-
-
+#Rock-Paper-Scissors Game: In this game, your bot will prompt the player to choose either rock, paper, or scissors. The bot will then randomly choose one of the options as well. The game will continue until the player decides to quit or until a predetermined number of rounds has been played. The player with the most wins at the end of the game is declared the winner.
 
 
 
