22839a54e42dc24098435e018c4158f5af03ec52
Ashane De <100096985+waltyy@users.noreply.github.com>
2023-03-25 23:58:32 +0000

    Added more comments to code

--- main.py
+++ main.py
@@ -8,17 +8,20 @@ TOKEN = os.environ['MY_TOKEN']
 intents = discord.Intents.default()
 intents.message_content = True
 
+#this code makes "." the start of the user input so that if a user says ".joke" the bot will know that the user is talking to it and will run the joke function. this also helps if the user says "joke" in a sentence, the bot won't run because the "." isn't infront of the joke.
 bot = commands.Bot(command_prefix=".", intents=intents)
 
+#this code turns the words "rock", "paper", "scissors" into emojis so that it will be easier for the user to read and know what the bot has responded with.
 icons = {"scissors":"✂️", 
           "paper": "🧾",
          "rock":"🗿"}
 
+#dice function that rolls a number from 1-6. The user sends a specific number of times the dice is supposed to rolled (for loop).
 @bot.command(name="roll")
 async def roll(ctx, num):
   for i in range(int(num)):
     d = random.randint(1, 6)
-    await ctx.channel.send(f"Rolling a D6 : {d}")
+    await ctx.channel.send(f"Rolling a Dice : {d}")
 
 
 @bot.command(name="ask")
@@ -33,6 +36,7 @@ async def ask(ctx, *words):
     await ctx.channel.send("That's not a question")
 
 
+#joke function:
 @bot.command(name="joke", aliases=["jokes", "j", "J"])  #aliases means it run the funtion if the user spells joke with an s or if the user simply puts /j
 async def joke(ctx):
   j = await Jokes()
@@ -62,11 +66,13 @@ async def joke(ctx):
 @bot.command(name = "rps")
 async def rps(ctx, message):
   answer = message.lower()
-  choices = ["rock", "paper", "scissors", "help"]
+  choices = ["rock", "paper", "scissors", "help", ""]
   computers_answer =  random.choice(choices)
   if answer not in choices:
     await ctx.channel.send("That is not an option in the game paper, scissors, rock.")
   elif answer == "help":
+    await ctx.channel.send("To play Rock, Paper, Scissors with me, enter '.rps rock/paper/scissors'")
+  elif answer == "":
     await ctx.channel.send("To play Rock, Paper, Scissors with me, enter '.rps rock/paper/scissors'")
   else:
     if computers_answer == answer:
